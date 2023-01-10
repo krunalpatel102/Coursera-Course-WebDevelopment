bbbcc55b3300174e757819ac6315c0e4a9952808
Dr Krunal Patel <it.krunalpatel@adit.ac.in>
2023-01-10 14:40:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 (function() {
     'use strict';
 
-angular.module('LunchCheckerApp', [])
-.controller('LunchCheckerController', LunchCheckerController);
+    angular.module('LunchCheckerApp', [])
+    .controller('LunchCheckerController', LunchCheckerController);
 
-LunchCheckerController.$inject = ['$scope'];
+    LunchCheckerController.$inject = ['$scope'];
 
-function LunchCheckerController($scope) {
-    $scope.noDishes = "";
-    $scope.message = "";
-    $scope.checked = false;
+    function LunchCheckerController($scope) {
+        $scope.noDishes = "";
+        $scope.message = "";
+        $scope.checked = false;
 
-    $scope.checkLunch = function() {
-        if ($scope.noDishes.trim().length === 0) {
-            $scope.empty = true;
-        } 
-        else {
-            $scope.checked = true;
-            $scope.empty = false;
-            var Dishes = $scope.noDishes.split(',');
-            var DishesWithoutEmptys = Dishes.filter(function(n) {
-                return n.trim() !== '';
-        });
-
-        if (DishesWithoutEmptys.length <= 3) {
-            $scope.message = 'Enjoy!';
-        } else {
-            $scope.message = 'Too much dishes entered!';
+        $scope.checkLunch = function() {
+            if ($scope.noDishes.trim().length === 0) {
+                $scope.empty = true;
+            } 
+            else {
+                $scope.checked = true;
+                $scope.empty = false;
+                var Dishes = $scope.noDishes.split(',');
+                var DishesWithoutEmptys = Dishes.filter(function(n) {
+                    return n.trim() !== '';
+            });
+            if (DishesWithoutEmptys.length <= 3) {
+                $scope.message = 'Enjoy!';
+            } 
+            else {
+                $scope.message = 'Too much dishes entered!';
             }
         }
         };
